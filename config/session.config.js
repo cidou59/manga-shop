04be7201123446693e385c76c7930384195055ad
VotreNom <nguyen.joseph00@gmail.com>
2025-04-17 22:52:21 +0200

    Ajout du fichier users.controller.js

--- config/session.config.js
+++ config/session.config.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
         maxAge: 1000 * 60 * 60 * 24 * 14 // 14 jours
       },
       store: MongoStore.create({
-        mongoUrl: process.env.DATABASE_URL, // URL de la base MongoDB
+        mongoUrl: process.env.DATABASE_URL, // URL de votre base MongoDB
         ttl: 60 * 60 * 24 * 14, // 14 jours
         autoRemove: 'native',
         touchAfter: 24 * 3600 // Réduit les écritures fréquentes
